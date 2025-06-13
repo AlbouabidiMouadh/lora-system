37d573d4fa1c783e20e37d254938c91f8591ac6f
AlbouabidiMouadh <albouabidimouadh@gmail.com>
2025-06-13 14:47:35 +0100

    Update pumpRoutes.js

--- server/routes/pumpRoutes.js
+++ server/routes/pumpRoutes.js
@@ -5,10 +5,10 @@ const { protect } = require('../middlewares/auth');
 
 router.post('/', protect, pumpController.createPump);
 router.get('/', protect, pumpController.getPumps);
+router.get('/user', protect, pumpController.getPumpsByUserId);
 router.get('/:id', protect, pumpController.getPumpById);
 router.put('/:id', protect, pumpController.updatePump);
 router.delete('/:id', protect, pumpController.deletePump);
-router.get('/user', protect, pumpController.getPumpsByUserId);
 router.patch('/:id/status', protect, pumpController.updatePumpStatus);
 
 module.exports = router;
